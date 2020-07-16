6c613db0471e459695b4617001d7b2df902f70d4
Aaron Brodersen <aaron@abrodersen.com>
2020-07-15 21:40:02 -0500

    enable reconnect

--- src/Server/ClientApp/src/components/Chat.js
+++ src/Server/ClientApp/src/components/Chat.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react'
 import { ChatInput } from './ChatInput'
 import ChatMessage from './ChatMessage'
 
+function getWsUri(id) {
+  let scheme = window.location.protocol == 'https:' ? 'wss' : 'ws';
+  let wsUri = scheme + '://' + window.location.host + '/api/rooms/' + id;
+  return wsUri;
+};
+
+
 export class Chat extends Component {
   constructor(props) {
     super(props);
     const { id } = props.match.params;
 
-    console.log('detected scheme: ' + window.location.protocol);
-    let scheme = window.location.protocol == 'https:' ? 'wss' : 'ws';
-    let wsUri = scheme + '://' + window.location.host + '/api/rooms/' + id;
-    console.log('connecting to ' + wsUri);
-    this.ws = new WebSocket(wsUri)
+    this.room = id;
 
     this.state = {
       name: 'Me',
@@ -19,8 +22,11 @@ export class Chat extends Component {
     };
   };
 
+  resetWebsocket() {
+    let wsUri = getWsUri(this.room);
+    console.log('connecting to ' + wsUri);
+    this.ws = new WebSocket(wsUri)
 
-  componentDidMount() {
     this.ws.onopen = () => {
       // on connecting, do nothing but log it to the console
       console.log('connected')
@@ -36,10 +42,12 @@ export class Chat extends Component {
     this.ws.onclose = () => {
       console.log('disconnected')
       // automatically try to reconnect on connection loss
-      this.setState({
-        ws: new WebSocket(URL),
-      })
+      this.resetWebsocket();
     }
+  }
+
+  componentDidMount() {
+    this.resetWebsocket();
   }
 
   addMessage = message =>
